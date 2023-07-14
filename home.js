c4d7c00b98d60a1a66bca6b95789c32f03cabb8d
shahzain46 <shahzain6016@gmail.com>
2023-07-14 05:20:48 -0700

    DSA in js

---
+++ home.js
@@ -0,0 +1,12 @@
+// let data = [2,43,56,7,87,9,76,78,32,56];
+
+// for(let i=0;i<data.length;i++){
+//     document.write(`Index of Arry ${[i]} is ${data[i]}<br>`)
+// }
+
+
+// function getEl() {
+//     let el = document.getElementById("number").value;
+//     alert(`${data[el]} is the value of index ${el}`)
+// }
+
